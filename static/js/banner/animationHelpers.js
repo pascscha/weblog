4669047763c33f2ea071c2481614f4b3ed31426b
Pascal Schärli <pascal@schaerli.org>
2026-06-26 20:06:55 +0200

    fix: improve noscript experience, inline svg and host google font myself

--- static/js/banner/animationHelpers.js
+++ static/js/banner/animationHelpers.js
@@ -6,22 +6,14 @@ async function getDiagramElements(diagramID) {
         }
     }
 
-    // Animate Padding oracle
-    await fetch('/img/AES-CBC.svg')
-        .then(response => response.text())
-        .then(data => {
-            // Inline the SVG
-            var diagram = document.getElementById(diagramID)
-            diagram.innerHTML = data;
+    var diagram = document.getElementById(diagramID)
+    var allTexts = diagram.querySelectorAll("text[id]");
+    allTexts.forEach(function (element) {
+        var match = element.id.match(/^([CIOP])([0-9A-F])$/);
+        if (match) {
+            diagramElements[match[1]][parseInt(match[2], 16)] = element;
+        }
+    })
 
-            // Find animated text elements by ID (e.g., "P0", "O1", "C2", "I3")
-            var allTexts = diagram.querySelectorAll("text[id]");
-            allTexts.forEach(function (element) {
-                var match = element.id.match(/^([CIOP])([0-9A-F])$/);
-                if (match) {
-                    diagramElements[match[1]][parseInt(match[2], 16)] = element;
-                }
-            })
-        })
     return diagramElements;
 }
